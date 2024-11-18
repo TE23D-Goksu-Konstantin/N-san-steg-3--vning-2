08adeeb0fac02ec742b0f72d2f3ad7b15032d4bc
Konstantin Göksu <konstantin.goksu@elev.ga.ntig.se>
2024-11-18 09:54:18 +0100

    klar

---
+++ Näsan steg 3 övning 2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Reflection.Metadata;
+
+class Program
+{
+
+    static void MinMetod(string namn, int år)
+    {
+        Console.WriteLine(namn + " är " + år + " gammal");        
+    }
+
+    static void Main(string[] args)
+    {
+    MinMetod("Erland", 24);
+    MinMetod("Bertil", 65);
+    MinMetod("Urban", 89);
+    Console.ReadLine();
+    }
+
+}
